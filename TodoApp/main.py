ac8284f475045cdc51b3b68ff45ac620f299cb2d
krzysztof <krzysztof.szymczyk@yahoo.com>
2024-09-03 13:23:44 +0200

    check for main.py

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -7,22 +7,22 @@ from .models import Base
 from .routers import auth, todos, admin, users
 
 app = FastAPI()
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:10000",
-    "https://fastapiapp-kmqo.onrender.com"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+#     "http://localhost:10000",
+#     "https://fastapiapp-kmqo.onrender.com"
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="TodoApp/static"), name="static")
