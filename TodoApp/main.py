6071812d281a34fd17f9f60d887ed11768f501d2
krzysztof <krzysztof.szymczyk@yahoo.com>
2024-09-03 13:36:35 +0200

    code cleanup

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,28 +1,13 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
+
 from .database import engine
 from .models import Base
 from .routers import auth, todos, admin, users
 
 app = FastAPI()
-# origins = [
-#     "http://localhost.tiangolo.com",
-#     "https://localhost.tiangolo.com",
-#     "http://localhost",
-#     "http://localhost:8080",
-#     "http://localhost:10000",
-#     "https://fastapiapp-kmqo.onrender.com"
-# ]
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+
 Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="TodoApp/static"), name="static")
