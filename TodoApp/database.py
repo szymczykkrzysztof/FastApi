c5cdd7c5c10ef53e45022b3e9a6fb8f8ca8b3ca1
krzysztof <krzysztof.szymczyk@yahoo.com>
2024-09-03 12:55:25 +0200

    database switched

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://deploy_db_ik7p_user:bdFluegY3iBMhgm0aIP0jiHE9fBxeCu3@dpg-crbekprqf0us73dbn2t0-a.frankfurt-postgres.render.com/deploy_db_ik7p'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
