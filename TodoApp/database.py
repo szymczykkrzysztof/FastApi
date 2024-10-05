c41380947cad40e9b9aeead3dd48f0e7a5af96ad
krzysztof <krzysztof.szymczyk@yahoo.com>
2024-10-05 20:11:36 +0200

    updated_database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://deploy_db_ik7p_user:bdFluegY3iBMhgm0aIP0jiHE9fBxeCu3@dpg-crbekprqf0us73dbn2t0-a.frankfurt-postgres.render.com/deploy_db_ik7p'
+SQLALCHEMY_DATABASE_URL = 'postgresql://tododb_gto0_user:9JisDnSwN3LXwAisZFBXRt29nyjFAuYx@dpg-cs0nut0gph6c73acb200-a.frankfurt-postgres.render.com/tododb_gto0'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
